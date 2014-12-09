fc769958d8f87cab77a5581d71a5ff8e4cd7fe50
ershov-ilya <ershov.ilya@gmail.com>
2014-12-09 14:16:15 +0300

    publishing

--- emarsys_api_connector.class.php
+++ emarsys_api_connector.class.php
@@ -7,13 +7,36 @@
  */
 
 class Emarsys_API_Connector {
-    public function __construct()
-    {
+    private $config,$username,$password,$api_url;
 
+    public function __construct($campaign, $pathToConfig='config/')
+    {
+        require($pathToConfig.'api.config.php');
+        $this->config=getConfig($campaign);
+        $this->username=$this->config['username'];
+        $this->password=$this->config['secret'];
+        $this->api_url=$this->config['suiteApiUrl'];
     }
-    function get($username,$password,$env,$uri,$params=array()){
-        $host = "https://$env.emarsys.net";
-        $url = $host."/api/v2/".$uri;
+
+    public function test()
+    {
+        //header('Content-Type: text/plain; charset=utf-8');
+
+        //print_r($this->get('event'));
+
+        /*
+        $list=array("a_s_w_4@mail.ru", "irishapo@gmail.com");
+        $params = array("keyId" => "3", "keyValues" => $list, "fields"=>array("1","2","3","31"));
+        $data_string = json_encode($params);
+        print_r($this->post("contact/getdata", $data_string));
+        /**/
+    }
+
+    /** @return stdClass */
+    function get($uri,$params=array()){
+        $url = $this->api_url.$uri;
+        $username=$this->username;
+        $password=$this->password;
 
         $url.=$this->paramsToString($params);
 
@@ -28,7 +51,7 @@ class Emarsys_API_Connector {
 
         $ch = curl_init($url);
 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);// You forgot to send the headers!
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
         curl_setopt($ch, CURLOPT_POST, false);
 
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // For Windows machines... some windows misses some CACERT.
@@ -37,21 +60,11 @@ class Emarsys_API_Connector {
         return  $output = json_decode(curl_exec($ch));
     }
 
-    function paramsToString($arr)
-    {
-        if(empty($arr)) return '';
-        $str='?';
-        foreach($arr as $key => $value)
-        {
-            $str.=$key.'='.$value.'&';
-        }
-        $str=preg_replace('/&$/','', $str);
-        return $str;
-    }
-
-    public function post($username,$password,$env,$uri,$data_string){
-        $host = "https://$env.emarsys.net";
-        $url = $host."/api/v2/".$uri;
+    /** @return stdClass */
+    public function post($uri,$data_string){
+        $url = $this->api_url.$uri;
+        $username=$this->username;
+        $password=$this->password;
 
         $created  = date('Y-m-d').'T'.date('H:i:s').'Z';
         $nonce = substr(md5(uniqid('nonce_', true)),0,16);
@@ -64,7 +77,7 @@ class Emarsys_API_Connector {
 
         $ch = curl_init($url);
 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);// You forgot to send the headers!
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 
@@ -74,9 +87,12 @@ class Emarsys_API_Connector {
         return  $output = json_decode(curl_exec($ch));
     }
 
-    public function put($username,$password,$env,$uri,$data_string){
-        $host = "https://$env.emarsys.net";
-        $url = $host."/api/v2/".$uri;
+    /** @return stdClass */
+    public function put($uri,$data_string){
+        $url = $this->api_url.$uri;
+        $username=$this->username;
+        $password=$this->password;
+
         $created  = date('Y-m-d').'T'.date('H:i:s').'Z';
         $nonce = substr(md5(uniqid('nonce_', true)),0,16);
         $passwordDigest = base64_encode(sha1($nonce . $created . $password));
@@ -100,8 +116,24 @@ class Emarsys_API_Connector {
         return $output;
     }
 
-    function emarsys_delete($username,$password,$env,$uri,$data_string){
+    function emarsys_delete($uri,$data_string){
+        $url = $this->api_url.$uri;
+        $username=$this->username;
+        $password=$this->password;
 
         return '';
     }
-} 
+
+    function paramsToString($arr)
+    {
+        if(empty($arr)) return '';
+        $str='?';
+        foreach($arr as $key => $value)
+        {
+            $str.=$key.'='.$value.'&';
+        }
+        $str=preg_replace('/&$/','', $str);
+        return $str;
+    }
+
+}
